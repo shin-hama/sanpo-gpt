75283e17e8b75e068b0efbf78bfb672a79ca216a
shin-hama <htysivista@gmail.com>
2023-07-01 12:43:09 +0900

    first commit

---
+++ functions/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['tsconfig.json', 'tsconfig.dev.json'],
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+  },
+  ignorePatterns: [
+    '/lib/**/*', // Ignore built files.
+  ],
+  plugins: ['@typescript-eslint', 'import'],
+  rules: {
+    'import/no-unresolved': 0,
+    indent: ['error', 2],
+  },
+}
